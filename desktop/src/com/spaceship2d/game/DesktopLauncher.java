a9ddd5bd67307394aaa257a9d56f7e482c5e1242
Eriklps <erikdslopes@gmail.com>
2023-11-07 16:55:18 -0300

    First commit

---
+++ desktop/src/com/spaceship2d/game/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.spaceship2d.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.spaceship2d.game.SpaceShip2d;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("SpaceShip2d");
+		config.setWindowedMode(1280, 720);
+		new Lwjgl3Application(new SpaceShip2d(), config);
+	}
+}
